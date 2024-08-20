bf69c13d1c60b612e820f3ad66da9bdd7dd3d09c
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2024-08-20 17:35:33 -0400

    removed junix

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,6 @@ val junixsocketVersion: String by project
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
     `maven-publish`
 }
 
@@ -25,11 +24,6 @@ sourceSets {
 val exampleImplementation by configurations
 exampleImplementation.extendsFrom(configurations.implementation.get())
 
-val shadowBundle by configurations.creating {
-    isCanBeConsumed = false
-    isCanBeResolved = true
-}
-
 dependencies {
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinVersion")
@@ -39,28 +33,11 @@ dependencies {
     // SLF4J
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
 
-    // JunixSocket
-    shadowBundle(implementation("com.kohlschutter.junixsocket:junixsocket-core:$junixsocketVersion")!!)
-
     // Example
     exampleImplementation(sourceSets.main.get().output)
     exampleImplementation("org.apache.logging.log4j:log4j-api:2.17.1")
     exampleImplementation("org.apache.logging.log4j:log4j-core:2.17.1")
     exampleImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
-}
-
-tasks {
-    shadowJar {
-        configurations = listOf(shadowBundle)
-        archiveClassifier.set("") // fix jitpack
-
-        minimize()
-    }
-
-    jar {
-        enabled = false
-        dependsOn(shadowJar)
-    }
 }
 
 publishing {
--- src/main/kotlin/dev/cbyrne/kdiscordipc/core/socket/impl/UnixSocket.kt
+++ src/main/kotlin/dev/cbyrne/kdiscordipc/core/socket/impl/UnixSocket.kt
@@ -3,27 +3,27 @@ package dev.cbyrne.kdiscordipc.core.socket.impl
 import dev.cbyrne.kdiscordipc.core.socket.RawPacket
 import dev.cbyrne.kdiscordipc.core.socket.Socket
 import dev.cbyrne.kdiscordipc.core.util.reverse
-import org.newsclub.net.unix.AFUNIXSocket
-import org.newsclub.net.unix.AFUNIXSocketAddress
 import java.io.DataInputStream
 import java.io.File
+import java.net.UnixDomainSocketAddress
 import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
 
 class UnixSocket : Socket {
-    private val socket = AFUNIXSocket.newInstance()
+    private val socket: SocketChannel = SocketChannel.open()
 
     override val connected: Boolean
         get() = socket.isConnected
 
     override fun connect(file: File) {
-        socket.connect(AFUNIXSocketAddress.of(file))
+        socket.connect(UnixDomainSocketAddress.of(file.toPath()))
     }
 
     override fun read(): RawPacket {
         val opcode = readLittleEndianInt()
         val length = readLittleEndianInt()
 
-        val stream = DataInputStream(socket.inputStream)
+        val stream = DataInputStream(socket.socket().getInputStream())
         val data = ByteArray(length)
         stream.readFully(data)
 
@@ -35,13 +35,13 @@ class UnixSocket : Socket {
 
     private fun readBytes(length: Int): ByteArray {
         val array = ByteArray(length)
-        socket.inputStream.read(array, 0, length)
+        socket.read(ByteBuffer.wrap(array))
 
         return array
     }
 
     override fun write(bytes: ByteArray) {
-        socket.outputStream.write(bytes)
+        socket.write(ByteBuffer.wrap(bytes))
     }
 
     override fun close() {
